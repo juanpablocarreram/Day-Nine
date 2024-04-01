be4c09c6b7dfbb7dd73217561649a4d27fa30ea9
Juan Pablo Carrera <140585461+juanpablocarreram@users.noreply.github.com>
2024-03-31 22:18:17 -0600

    Feat: Add finished files

---
+++ main.js
@@ -0,0 +1,40 @@
+const rainDropTop = document.querySelectorAll(".rainDropTop");
+const rainDropMiddle = document.querySelectorAll(".rainDropMiddle");
+const rainDropBottom = document.querySelectorAll(".rainDropBottom");
+let exponentialPositionTop = 0;
+let exponentialPositionBottom = 20;
+let exponentialDelayMiddle = .1;  
+let exponentialDelay = 0;
+let i = 0;
+let startTime = 0;
+let repetitions = 0;
+for(let i= 0; i<rainDropTop.length; i++){
+    rainDropBottom[i].style.left = exponentialPositionBottom + "px";
+    rainDropTop[i].style.right = exponentialPositionTop + "px";
+    rainDropMiddle[i].style.left = exponentialPositionTop + "px";
+    rainDropBottom[i].style.animation = "rainDrops 1.2s " + exponentialDelayMiddle + "s infinite linear";
+    rainDropTop[i].style.animation  = "rainDrops 1.2s " + exponentialDelay +"s infinite linear";
+    rainDropMiddle[i].style.animation  = "rainDrops 1.2s " + exponentialDelay +"s infinite linear";
+    exponentialDelay = exponentialDelay + .3;
+    exponentialDelayMiddle = exponentialDelayMiddle +.2;
+    exponentialPositionTop = exponentialPositionTop + 80;
+    exponentialPositionBottom = exponentialPositionBottom + 80;
+    
+}
+function appearStar(){
+    i++;
+    console.log(i);
+
+}
+function getTime(){
+    while(true){
+        let currentTime = performance.now();
+        let elapsedTime = currentTime - startTime;
+        if (currentTime >= 1200){
+            repetitions++;
+            console.log(repetitions);
+        }
+    } 
+}
+rainDropTop[3].addEventListener("animationend", appearStar);
+getTime();
